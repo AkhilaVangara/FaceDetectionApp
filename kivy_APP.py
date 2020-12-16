39d877a8b07c9ffe5a8a7d0da4fde8c9dff6c46a
Akhila Vangara <akhila.vangara@hec.edu>
2020-12-16 22:47:57 +0100

    Added a few more features and filters

--- kivy_APP.py
+++ kivy_APP.py
@@ -16,20 +16,24 @@ import cv2
 class CamApp(App):
     def build(self):
         layout = RelativeLayout(size=(500,500))
-        self.cam = Camera(play=True,resolution=(2000,2000))
-        self.button = Button(text='Click a photo',
+        self.cam = Camera(play=True,resolution=(1500,1500))
+        self.button = Button(text='Detect a face!',
                         size_hint=(.5, .07),
                         pos=(450,100))
         self.button.bind(on_press=self.on_press_button)
- #       self.textinput = TextInput(text='Enter Image Name')
- #       self.textinput.bind(on_text_validate=self.on_enter)
+        self.button_sketch = Button(text='Create a sketch!',
+                        size_hint=(.5, .07),
+                        pos=(450,10))
+        self.button_paint = Button(text='Create a painting',
+                        size_hint=(.5,0.07),
+                        pos=(450,190))
+        self.button_sketch.bind(on_press=self.on_press_sketch_button)
+        self.button_paint.bind(on_press=self.on_press_paint_button)
         layout.add_widget(self.cam)
         layout.add_widget(self.button)
+        layout.add_widget(self.button_sketch)
+        layout.add_widget(self.button_paint)
         return layout
-
-#    def on_enter(self,instance):
-#        self.imagePath = TextInput.text()
-#        return 0
 
     def on_press_button(self,instance):
  #       try: 
@@ -47,8 +51,6 @@ class CamApp(App):
     minSize=(30, 30),
     flags = cv2.CASCADE_SCALE_IMAGE
 )
-
-
 # Draw a rectangle around the faces
         for (x, y, w, h) in faces:
             cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
@@ -57,5 +59,27 @@ class CamApp(App):
 
         return 0
 
+
+    def on_press_sketch_button(self,instance):
+        self.imagePath = os.path.join('Sketch','Selfie_sketch.png')
+        self.cam.export_to_png(self.imagePath)
+        image = cv2.imread(self.imagePath)
+        img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        img_gray_inv = 255 - img_gray
+        img_blur = cv2.GaussianBlur(img_gray_inv, ksize=(21, 21),
+                            sigmaX=0, sigmaY=0)
+        img_blend = cv2.divide(img_gray, 255-img_blur, scale=256)
+        cv2.imwrite(os.path.join('Sketch','transformed.png'),img_blend) 
+
+        return 0
+
+    def on_press_paint_button(self,instance):
+        self.imagePath = os.path.join('Paint','Selfie.png')
+        self.cam.export_to_png(self.imagePath)
+        img = cv2.imread(self.imagePath)
+        res = cv2.stylization(img, sigma_s=30, sigma_r=0.3)
+        cv2.imwrite(os.path.join('Paint','painting.png'),res)
+
+        return 0
 if __name__ == '__main__':
     CamApp().run()
